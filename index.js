cc0318aae3eacd7f114384989615edd98cfae040
Amit1045 <134228101+Amit1045@users.noreply.github.com>
2024-08-31 15:06:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const button=document.querySelector(".calculate")
+button.addEventListener("click",function(e){
+    
+const Userheight=document.querySelector("#userheight").value;
+const Userweight=document.querySelector("#userweight").value;
+
+    const bmi=(((703*Userweight/(Userheight*Userheight))*10)).toFixed(2)
+          if(bmi<18.5)
+          {
+            document.getElementById("result").innerHTML=`Your BMI is: ${bmi},  so you are underweight.`
+          }else if(bmi >= 18.5 && bmi < 24.9){
+            document.getElementById("result").innerHTML=`Your BMI is: ${bmi}, so your weight is normal.` 
+          }else if(bmi>24.9)
+          {
+            document.getElementById("result").innerHTML=`Your BMI is ${bmi}, so you are Overweight.`
+          }
+})
+
+
